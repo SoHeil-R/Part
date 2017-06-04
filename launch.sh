5c150a81ee53807d682461a866b341438dad6c34
SoHeil-R <soheildkta@gmail.com>
2017-06-04 15:19:46 +0430

    #PartPlus

---
+++ launch.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+cd $HOME/Part
+
+install() {
+	    cd libs
+		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+		sudo apt-get install g++-4.7 -y c++-4.7 -y
+		sudo apt-get update
+		sudo apt-get upgrade
+		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+		sudo apt-get install screen -y
+		sudo apt-get install tmux -y
+		sudo apt-get install libstdc++6 -y
+		sudo apt-get install lua-lgi -y
+		sudo apt-get install libnotify-dev -y
+		wget https://valtman.name/files/telegram-cli-1222
+		mv telegram-cli-1222 tgcli
+		chmod +x tgcli
+		cd ..
+		chmod +x bot
+}
+
+red() {
+  printf '\e[1;31m%s\n\e[0;39;49m' "$@"
+}
+green() {
+  printf '\e[1;32m%s\n\e[0;39;49m' "$@"
+}
+white() {
+  printf '\e[1;37m%s\n\e[0;39;49m' "$@"
+}
+update() {
+	git pull
+}
+
+if [ "$1" = "install" ]; then
+	install
+elif [ "$1" = "update" ]; then
+	update
+else
+if [ ! -f ./libs/tgcli ]; then
+    echo "tgcli not found"
+    echo "Run $0 install"
+    exit 1
+fi
+	#sudo service redis-server restart
+	./libs/tgcli -s ./bot/bot.lua $@
+fi
